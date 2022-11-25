cfc026c798ac8a95a7c81c751d00332042c3eb63
BahaaalHalabi01 <67575710+BahaaalHalabi01@users.noreply.github.com>
2022-11-26 00:40:04 +0300

    add random pokemon clientside - hydration error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,25 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon"
 import { trpc } from "@/utils/trpc"
+
 
 export default function Home() {
 
   const {data,isLoading} = trpc.hello.useQuery({text:'Bahaa'})
 
-  if(isLoading) return <div>Loading...</div>
+  const [first,second] = getOptionsForVote()
 
-  if (data) return <div> {data.greeting}</div>
 
   return (
    <div className='h-screen w-screen flex flex-col justify-center items-center'>
     <div className=' text-2xl text-center '>Which Pokemon is rounder ?</div>
     <div className='border rounded p-8 flex justify-between max-w-3xl items-center mt-2'>
-      <div className='w-16 h-16 bg-red-200'></div>
-    <div className='p-8'></div>
-      <div className='w-16 h-16 bg-red-200'></div>
-
+      <div className='w-16 h-16 bg-red-800'>{first}</div>
+      <div className='p-8'>Vs</div>
+      <div className='w-16 h-16 bg-red-800'>{second}</div>
       </div>
-    
     </div>
     
   )
 }
+
+
