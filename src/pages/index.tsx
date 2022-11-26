246687b497e47e6bc4f3a97b9276671407eced9e
BahaaalHalabi01 <67575710+BahaaalHalabi01@users.noreply.github.com>
2022-11-26 23:50:53 +0300

    add voting mutation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,9 +15,18 @@ export default function Home() {
   const firstPokemon = trpc['get-pokemon-by-id'].useQuery({ id: first })
   const secondPokemon = trpc['get-pokemon-by-id'].useQuery({ id: second })
 
+  const voteMutation = trpc['cast-vote'].useMutation()
+
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null
 
   const voteForRoundest = (selected: number) => {
+
+    if (selected === first) {
+      voteMutation.mutate({ votedFor: first, votedAgainst: second })
+    } else {
+      voteMutation.mutate({ votedFor: second, votedAgainst: first })
+    }
+
     setIds(getOptionsForVote())
   }
 
